9119151b6871bc9610a4ba2ba8ecaf9423a29cbf
hussam Ibraheem <h.ibraheem@outlook.com>
2024-03-20 05:27:04 +0300

    btn-up

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ 
+  let btn = document.querySelector(`.btn-up`);
+  window.onscroll = function () {
+    if (window.scrollY >= 600) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  btn.onclick = function () {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+ 
