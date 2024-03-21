a56e360f88f21d45d75bb2e7906d57be1beff2ab
hussam Ibraheem <h.ibraheem@outlook.com>
2024-03-22 00:20:41 +0300

    dynamic Landing Image

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
- 
-  let btn = document.querySelector(`.btn-up`);
-  window.onscroll = function () {
-    if (window.scrollY >= 600) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  };
+//  dynamic background
 
-  btn.onclick = function () {
-    window.scrollTo({
-      left: 0,
-      top: 0,
-      behavior: "smooth",
-    });
-  };
- 
+let imgsArray = [
+  "https://images.unsplash.com/photo-1484665739383-a1069a82d4be?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1487284122274-e864e9dec2bf?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1540821924489-7690c70c4eac?q=80&w=2601&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+let landingPage = document.querySelector(".landing");
+
+setInterval(() => {
+  let randomNum = Math.floor(Math.random() * imgsArray.length);
+  landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
+}, 5000);
+
+// To up Btn
+let btn = document.querySelector(`.btn-up`);
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
